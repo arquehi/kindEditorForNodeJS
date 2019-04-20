c6bea041ceabe88fe460e5fb3c245e76845f173e
arquehi <arquehi@gmail.com>
2019-04-20 19:40:48 +0800

    add file upload dir add video upload dir fix ie11
    Signed-off-by: arquehi <arquehi@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var os = require('os');
 var formidable = require('formidable');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'KindEditor For NodeJS' });
 });
 
-router.post('/uploadImg', function(req, res, next) {
+router.post('/uploadImg', function (req, res, next) {
   var form = new formidable.IncomingForm();
   form.keepExtensions = true;
   let ActionType = req.query.dir;
-  if (ActionType === 'image' || ActionType === 'file' || ActionType === 'media'|| ActionType === 'flash') {
+  if (ActionType === 'image' || ActionType === 'file' || ActionType === 'media' || ActionType === 'flash') {
     switch (ActionType) {
       case 'image':
         form.uploadDir = __dirname + '/../public/Upload/Images';//图片
@@ -31,29 +32,37 @@ router.post('/uploadImg', function(req, res, next) {
       throw err;
     }
 
+    console.log(files.imgFile.path);
     let file_url;
-    if (ActionType === 'image' || ActionType === 'file' || ActionType === 'media'|| ActionType === 'flash') {
+    if (ActionType === 'image' || ActionType === 'file' || ActionType === 'media' || ActionType === 'flash') {
       var image = files.imgFile;
       var path = image.path;
       path = path.replace('/\\/g', '/');
+      var op = os.type();
+      if (op == 'Windows_NT') {
+        file_url = path.substr(path.lastIndexOf("\\"), path.length);
+        file_url = file_url.replace("\\", "/");
+      }
       switch (ActionType) {
         case 'image':
-          file_url = '/Upload/Images' + path.substr(path.lastIndexOf('/'), path.length);//图片
-              break;
+          file_url = '/Upload/Images' + file_url;//图片
+          break;
         case 'file':
-          file_url = '/Upload/Files' + path.substr(path.lastIndexOf('/'), path.length); //附件
-              break;
+          file_url = '/Upload/Files' + file_url; //附件
+          break;
         case 'media':
         case 'flash':
-          file_url = '/Upload/Videos'+ path.substr(path.lastIndexOf('/'), path.length);//视频
-              break;
+          file_url = '/Upload/Videos' + file_url;//视频
+          break;
       }
     }
 
+    console.log(file_url);
     var info = {
       "error": 0,
       "url": file_url
     };
+    res.set('Content-Type', 'text/html');
     res.send(info);
   });
 });
